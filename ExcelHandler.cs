58dc6e949b47d3bcda8a65b0cc0ef77d695f1373
FJun <50333046+GitaJun@users.noreply.github.com>
2021-06-24 13:49:07 +0800

    Add files via upload

---
+++ ExcelHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using ClosedXML.Excel;
+
+namespace WorkChargerMigration
+{
+    /// <summary>
+    /// Excel处理器
+    /// </summary>
+    public class ExcelHandler
+    {
+        private readonly string excelPath;
+        /// <summary>
+        /// 创建Excel处理器，文件类型不符合会抛出异常
+        /// </summary>
+        /// <param name="excelPath">Excel文件绝对路径</param>
+        public ExcelHandler(string excelPath)
+        {
+            int dotIndex = excelPath.LastIndexOf('.');
+            string suffix = excelPath.Substring(dotIndex);
+            if (!new SortedSet<string>() { ".xls", ".XLS", ".xlsx", ".XLSX" }.Contains(suffix))
+                throw new Exception("请选择Excel类型文件（.xls或.xlsx文件）");
+            this.excelPath = excelPath;
+        }
+
+        /// <summary>
+        /// 从Excel表中获取数据
+        /// </summary>
+        /// <param name="sheet">工作表页数（从1开始）</param>
+        /// <param name="handle">对工作表的读写操作</param>
+        public void Handle(int sheet, Action<IXLWorksheet> handle)
+        {
+            using (XLWorkbook workbook = new(this.excelPath))
+            {
+                IXLWorksheet worksheet = workbook.Worksheet(sheet);  //某个sheet
+                handle(worksheet);
+            }
+        }
+    }
+}
---
+++ SqlServerDBHandler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace WorkChargerMigration
+{
+    /// <summary>
+    /// SqlServer数据库处理器
+    /// </summary>
+    public class SqlServerDBHandler
+    {
+        private readonly string server, uid, pwd, database;
+        /// <summary>
+        /// 创建SqlServer数据库处理器
+        /// </summary>
+        /// <param name="server">服务器名</param>
+        /// <param name="uid">账号</param>
+        /// <param name="pwd">密码</param>
+        /// <param name="database">数据库名</param>
+        public SqlServerDBHandler(string server, string uid, string pwd, string database)
+        {
+            this.server = server;
+            this.uid = uid;
+            this.pwd = pwd;
+            this.database = database;
+        }
+
+        /// <summary>
+        /// 从数据库中获取数据，若执行的SQL语句结果无数据，则抛出异常
+        /// </summary>
+        /// <param name="sql">sql语句</param>
+        /// <param name="read">委托：利用闭包，操作通过执行数据库语句创造的SqlDataReader对象，进行数据读取</param>
+        /// <returns>数据载体</returns>
+        public void GetData(string sql, Action<SqlDataReader> read)
+        {
+            using (SqlConnection connection = new SqlConnection($"server={server};uid={uid};pwd={pwd};database={database}"))
+            {
+                connection.Open();
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = sql;
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (!reader.HasRows) throw new Exception("数据库中无数据");
+                        read(reader);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 更新数据库数据
+        /// </summary>
+        /// <param name="sql">sql语句</param>
+        /// <returns>更新行数</returns>
+        public int UpdateData(string sql)
+        {
+            using (SqlConnection connection = new SqlConnection($"server={server};uid={uid};pwd={pwd};database={database}"))
+            {
+                connection.Open();
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = sql;
+                    return command.ExecuteNonQuery();
+                }
+            }
+        }
+
+    }
+}
