3388321f981c5e9f5a6fddbac9db74e92a227258
FJun <50333046+GitaJun@users.noreply.github.com>
2021-06-24 17:51:46 +0800

    Add files via upload

---
+++ SupportedSuffix.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JunUtility
+{
+    /// <summary>
+    /// 支持的文件后缀类
+    /// </summary>
+    public class SupportedFileSuffix : IDisposable
+    {
+        private readonly SortedSet<string> supportedSuffix;
+
+        /// <summary>
+        /// 创建一个支持的文件后缀类对象
+        /// </summary>
+        /// <param name="lowercaseSuffixes">带点号小写的后缀名</param>
+        public SupportedFileSuffix(params string[] lowercaseSuffixes)
+        {
+            supportedSuffix = new(lowercaseSuffixes);
+            List<string> tempUpper = new();
+            foreach (var item in supportedSuffix)
+            {
+                tempUpper.Add(item.ToUpper());
+            }
+            foreach (var item in tempUpper)
+            {
+                supportedSuffix.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// 校验文件是否是支持的类型
+        /// </summary>
+        /// <param name="fileName">文件名</param>
+        /// <returns>是否支持该类型的文件</returns>
+        public bool CheckFileSuffix(string fileName)
+        {
+            int dotIndex = fileName.LastIndexOf('.');
+            string suffix = fileName.Substring(dotIndex);
+            return supportedSuffix.Contains(suffix);
+        }
+
+        public void Dispose()
+        {
+            supportedSuffix.Clear();
+        }
+
+        public override string ToString()
+        {
+            string result = string.Empty;
+            foreach (var item in supportedSuffix)
+            {
+                result += $"'{item}'";
+            }
+            return result;
+        }
+    }
+}
